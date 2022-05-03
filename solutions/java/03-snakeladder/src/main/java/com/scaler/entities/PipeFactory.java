b5657bbc61630870ece5f11e7e7691d9fe7caefd
Arnav Gupta <championswimmer@gmail.com>
2022-05-04 00:15:23 +0530

    fix the logic to generate snakes and ladders
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

--- solutions/java/03-snakeladder/src/main/java/com/scaler/entities/PipeFactory.java
+++ solutions/java/03-snakeladder/src/main/java/com/scaler/entities/PipeFactory.java
@@ -24,16 +24,16 @@ public class PipeFactory {
             int startRow = 1 + random.nextInt(boardSize - 1);
             int endRow = random.nextInt(startRow);
 
-            start = boardSize * startRow + random.nextInt(boardSize);
-            end = boardSize * endRow + random.nextInt(boardSize) + 1;
+            start = boardSize * startRow + random.nextInt(boardSize - 1) + 1;
+            end = boardSize * endRow + random.nextInt(boardSize -1 ) + 1;
             return new Snake(start, end);
 
         } else { // pipeType == PipeType.LADDER
             int endRow = 1 + random.nextInt(boardSize - 1);
             int startRow = random.nextInt(endRow);
 
-            start = boardSize * startRow + random.nextInt(boardSize) + 1;
-            end = boardSize * endRow + random.nextInt(boardSize);
+            start = boardSize * startRow + random.nextInt(boardSize - 1) + 1;
+            end = boardSize * endRow + random.nextInt(boardSize - 1 );
             return new Ladder(start, end);
         }
     }
--- solutions/java/03-snakeladder/src/test/java/com/scaler/entities/BoardTests.java
+++ solutions/java/03-snakeladder/src/test/java/com/scaler/entities/BoardTests.java
@@ -1,5 +1,6 @@
 package com.scaler.entities;
 
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -13,14 +14,15 @@ public class BoardTests {
     }
 
     @Test
+    @RepeatedTest(30)
     void boardPipesAreWithinBoardBounds() {
-        Board board = new Board.Builder(10).build();
+        Board board = new Board.Builder(5).build();
 
         board.getPipes().forEach((start, pipe) -> {
             assertTrue(pipe.getStart() >= 1, "pipe start = " + pipe.getStart());
-            assertTrue(pipe.getStart() < 100, "pipe start = " + pipe.getStart());
+            assertTrue(pipe.getStart() < 25, "pipe start = " + pipe.getStart());
             assertTrue(pipe.getEnd() >= 1, "pipe end = " + pipe.getEnd());
-            assertTrue(pipe.getEnd() < 100, "pipe end = " + pipe.getEnd());
+            assertTrue(pipe.getEnd() < 25, "pipe end = " + pipe.getEnd());
         });
     }
 
